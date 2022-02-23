9991abb468bbe6d866eb5a237a5c2fd75136af22
Kiên <fukz2803@gmail.com>
2022-02-23 23:03:46 +0700

    a

--- bai-tap/bai-04/cau-1-library.sql
+++ bai-tap/bai-04/cau-1-library.sql
@@ -1,11 +1,14 @@
+-- Lấy ra các quyển sách được xuất trước năm 1900
 SELECT b.title , b.year_of_publication 
 FROM book b
 WHERE b.year_of_publication < 1900
 
+-- Lấy ra các quyển sách có số lượng trang dưới 100
 SELECT b.title , b.page_number 
 FROM book b 
 WHERE b.page_number < 100
 
+-- Liệt kê các quyển sách có id_publisher = 1 và sắp xếp theo thứ tự tăng dần của số trang
 SELECT b.title , b.id_publisher , b.page_number 
 FROM book b
 WHERE b.id_publisher = 1
--- bai-tap/bai-04/cau-2-sakila.sql
+++ bai-tap/bai-04/cau-2-sakila.sql
@@ -1,15 +1,18 @@
+-- Tính trung bình cộng rental_rate của các bộ phim có rating là R
 SELECT JSON_ARRAYAGG(
 JSON_OBJECT('title', f.title, 'rating', f.rating, 'rental_rate',f.rental_rate)) AS film_info,
 AVG(f.rental_rate) AS Avarage_rental_rate
 FROM film f 
-WHERE f.rating = 'G'
+WHERE f.rating = 'R'
 
+-- Đếm các bộ phim có special_features là Deleted Scenes
 SELECT JSON_ARRAYAGG(
 JSON_OBJECT('title',f.title ,'special_features', f.special_features)) AS film_info,
 COUNT(f.film_id) AS count_film
 FROM film f 
 WHERE f.special_features LIKE '%Deleted Scenes%'
 
+-- Tìm các bộ phim theo tên có chứa ‘ACADEMY’
 SELECT f.title 
 FROM film f 
 WHERE f.title LIKE '%ACADEMY%'
